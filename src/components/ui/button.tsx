493e24dc249392566e74538fd707999e1594dc91
fast-webtools <simon@hennig030.de>
2025-01-24 11:29:31 +0100

    Create button.tsx

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button: React.FC = ({ children, ...props }) => {
+  return (
+    <button {...props}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
